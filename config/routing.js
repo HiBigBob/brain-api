dc9f887153a17c18d5d0a4476266db618be05732
Adrien Bouttier <hibigbob@gmail.com>
2015-07-17 08:53:05 +0200

    Change routing

--- config/routing.js
+++ config/routing.js
@@ -2,10 +2,18 @@ var index = require('../controllers');
 var user = require('../controllers/user');
 var setup = require('../controllers/setup');
 var token = require('../controllers/token');
+var tasks = require('../controllers/tasks');
+
+var jwtAuth = require('../lib/auth');
+var requireAuth = require('../lib/require');
 
 module.exports.set = function(app) {
+	app.all('/api/*', [jwtAuth, requireAuth]);
+
+	app.use('/authenticate', token);
+	app.use('/setup', setup);
+
 	app.use('/api', index);
 	app.use('/api/user', user);
-	app.use('/api/authenticate', token);
-	app.use('/setup', setup);
+	app.use('/api/tasks', tasks);
 }
